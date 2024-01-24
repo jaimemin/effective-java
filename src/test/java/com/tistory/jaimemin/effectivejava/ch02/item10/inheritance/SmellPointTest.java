ad2b1f6efc509f0c55cf87d32ebc0823d2ff6ac2
jaimemin <jaimemin@gmail.com>
2024-01-25 07:13:07 +0900

    item10 - equals는 읿반 규약을 지켜 재정의하라

---
+++ src/test/java/com/tistory/jaimemin/effectivejava/ch02/item10/inheritance/SmellPointTest.java
@@ -0,0 +1,16 @@
+package com.tistory.jaimemin.effectivejava.ch02.item10.inheritance;
+
+import com.tistory.jaimemin.effectivejava.ch02.item10.Color;
+
+public class SmellPointTest {
+
+	/**
+	 * TODO -Xss10M
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SmellPoint p1 = new SmellPoint(1, 0, "sweat");
+		ColorPoint p2 = new ColorPoint(1, 0, Color.RED);
+		p1.equals(p2);
+	}
+}
