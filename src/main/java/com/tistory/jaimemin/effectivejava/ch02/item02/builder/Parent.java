4bc390f6cb6c4f14c6b4730d852766554d32c5bd
jaimemin <jaimemin@gmail.com>
2024-02-14 09:45:28 +0900

    아이템 2 빌더 업데이트

--- src/main/java/com/tistory/jaimemin/effectivejava/ch02/item02/builder/Parent.java
+++ src/main/java/com/tistory/jaimemin/effectivejava/ch02/item02/builder/Parent.java
@@ -1,11 +1,16 @@
 package com.tistory.jaimemin.effectivejava.ch02.item02.builder;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 @ToString
+@NoArgsConstructor
 @SuperBuilder(builderMethodName = "")
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Parent {
 
 	@NonNull
