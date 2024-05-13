4cea37a1bda87368ae67cacf053d5aa3d3014cd4
jaimemin <jaimemin@gmail.com>
2024-05-14 02:17:01 +0900

    12장 87 업로드

---
+++ src/main/java/com/tistory/jaimemin/effectivejava/ch12/item87/StringList.java
@@ -0,0 +1,57 @@
+package com.tistory.jaimemin.effectivejava.ch12.item87;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+// StringList with a reasonable custom serialized form  - Page 349
+public final class StringList implements Serializable {
+
+	private transient int size = 0;
+
+	private transient Entry head = null;
+
+	// No longer Serializable!
+	private static class Entry {
+		String data;
+		Entry next;
+		Entry previous;
+	}
+
+	// Appends the specified string to the list
+	public final void add(String s) {
+	}
+
+	/**
+	 * Serialize this {@code StringList} instance.
+	 *
+	 * @serialData The size of the list (the number of strings
+	 * it contains) is emitted ({@code int}), followed by all of
+	 * its elements (each a {@code String}), in the proper
+	 * sequence.
+	 */
+	private void writeObject(ObjectOutputStream s)
+		throws IOException {
+		s.defaultWriteObject();
+		s.writeInt(size);
+
+		// Write out all elements in the proper order.
+		for (Entry e = head; e != null; e = e.next) {
+			s.writeObject(e.data);
+		}
+	}
+
+	private void readObject(ObjectInputStream s)
+		throws IOException, ClassNotFoundException {
+		s.defaultReadObject();
+		int numElements = s.readInt();
+
+		// Read in all elements and insert them in list
+		for (int i = 0; i < numElements; i++) {
+			add((String)s.readObject());
+		}
+	}
+
+	// Remainder omitted
+}
